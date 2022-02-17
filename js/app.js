d64f0e30f3c87de8981df22428b53a2cee42a6d9
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-02-17 20:28:21 +0600

    js file

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 function getInput(elementId){
     const elementInput = document.getElementById(elementId);
     const elementInputValue = parseFloat(elementInput.value) ;
-    elementInput.value = "";
+    // elementInput.value = 0;
     return elementInputValue;
 }
-// calculate button
+// calculate income
 document.getElementById("calculate-btn").addEventListener("click", function(){
    const totalExpensesInput = document.getElementById("total-expenses");
 //   Total Expenses   
    const totalExpensesValue = getInput("food") + getInput("rent") + getInput("clothes"); 
 //    Balance
+    
     const incomeValue  = getInput("income");
     const balance = document.getElementById("balance");
     const failureMsg = document.getElementById("failure-msg");
-    if((incomeValue > totalExpensesValue && incomeValue > 0 && totalExpensesValue > 0))
+     if((incomeValue > totalExpensesValue && incomeValue > 0 && totalExpensesValue > 0))
     {
         totalExpensesInput.innerText = totalExpensesValue;
         balance.innerText = incomeValue - totalExpensesValue;
@@ -22,5 +23,27 @@ document.getElementById("calculate-btn").addEventListener("click", function(){
         failureMsg.style.display = "block";
     }
 })
+// calculate saving
+document.getElementById("save-btn").addEventListener("click", function(){
+    const income = getInput("income");
+    const saveInput = getInput("save-input");
+    const failureMsg = document.getElementById("failure-msg");
+    const savingAmount = document.getElementById("saving-amount");
+    const totalSaving = income * (saveInput / 100);
+    savingAmount.innerText = totalSaving;
+    // Remaining Balance part
+    const balance = document.getElementById("balance");
+    const balanceValue = parseFloat(balance.innerText);
+    // remaining balance
+    document.getElementById("remaining-balance").innerText =  balanceValue - parseFloat(savingAmount.innerText);
+    if(isNaN(totalSaving)){
+        failureMsg.style.display = "block";
+    }
+    else if(savingAmount.innerText > balanceValue){
+        failureMsg.style.display = "block";
+    }
+    
+    
 
+})
 
