70e3a6d0c12c83e978c74de3074187142fbf164e
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-02-17 16:30:37 +0600

    apply js in calculate button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
+function getInput(elementId){
+    const elementInput = document.getElementById(elementId);
+    const elementInputValue = parseFloat(elementInput.value) ;
+    elementInput.value = "";
+    return elementInputValue;
+}
 // calculate button
 document.getElementById("calculate-btn").addEventListener("click", function(){
-    const income = document.getElementById("income-value");
-    const addTk = document.getElementById("add-tk");
-    addTk.innerText = income.value;
-    income.value = "";
+   const totalExpensesInput = document.getElementById("total-expenses");
+//   Total Expenses   
+   const totalExpensesValue = getInput("food") + getInput("rent") + getInput("clothes"); 
+//    Balance
+    const incomeValue  = getInput("income");
+    const balance = document.getElementById("balance");
+    const failureMsg = document.getElementById("failure-msg");
+    if((incomeValue > totalExpensesValue && incomeValue > 0 && totalExpensesValue > 0))
+    {
+        totalExpensesInput.innerText = totalExpensesValue;
+        balance.innerText = incomeValue - totalExpensesValue;
+    }
+    else{
+        failureMsg.style.display = "block";
+    }
 })
+
+
